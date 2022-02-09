0fd1038c5ab25465de0609a07dc8c8ba1cd66cb1
gabriel <gabo27ajm@gmail.com>
2022-02-09 16:21:11 -0500

    primeros pasos

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -182,7 +182,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom(){
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-   return Math.round (0.6)
+   return Math.random()
 }
 
 function esPositivo(numero) {
@@ -190,13 +190,20 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if(numero > 0){
+    return  "Es positivo";}
+    if (numero < 0) {
+    return  "Es negativo";}
+  else{return false;}
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  
+
+
 }
 
 function combinarNombres(nombre, apellido) {
