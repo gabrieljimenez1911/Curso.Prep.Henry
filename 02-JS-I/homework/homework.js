6afac7c868eee0f33744a848b52c9e01d8f7a841
gabriel <gabo27ajm@gmail.com>
2022-06-10 09:27:18 -0500

    asi

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -201,29 +201,35 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return "hello world"+ "!";
   
-t 
+
 
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
-  // Tu código:
+  // Tu código: 
   
+  return "nombre" + " apellido";
+
+
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  return 'hola'+' martin'+'!';
   
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  return (alto*ancho) 
+  return (altoancho) 
+  
 }
 
 
